897b8ade34ee4937f815b0b9a258f9c7b0d9cbd6
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2025-05-14 15:43:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const { fishMenu } = require("./restaurant.js")
 
-
+/*
+  Do not comment out this code. 
+  
+  Your job is to make this small algorithm work by
+  writing supporting functionality in the other 
+  modules
+*/
 const dailyPriceLimit = 3.99
 const menu = fishMenu(dailyPriceLimit)
 console.log(menu)
